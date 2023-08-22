410d52a790ccda97ee4ec4c95a5ee3813efaa668
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-22 20:44:51 -0300

    creating folders and files

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form'
+
+const WeatherApp = () => {
+  return (
+    <>
+    <main className="dos-columnas">
+        <Form/>
+    </main>
+    </>
+  )
+}
+
+export default WeatherApp
