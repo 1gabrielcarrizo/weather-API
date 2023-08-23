fa096a0644eeeff9cc711834c6ff8435d48e92e1
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-22 22:08:59 -0300

    testing the API

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useWeather from '../hooks/useWeather'
 
 const Form = () => {
+
+  const [alerta, setAlerta] = useState('')
+
+  const { busqueda, datosBusqueda, consultarClima } = useWeather()
+
+  const { ciudad, pais } = busqueda
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(Object.values(busqueda).includes('')){
+      setAlerta('Todos los campos son obligatorios')
+      return
+    }
+    consultarClima(busqueda)
+  }
+
   return (
     <div className="contenedor">
-      <form>
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
         <div className="campo">
           <label htmlFor="ciudad">Ciudad:</label>
-          <input type="text" id='ciudad' name='ciudad' />
+          <input type="text" id='ciudad' name='ciudad' onChange={datosBusqueda} value={ciudad} />
         </div>
         <div className="campo">
           <label htmlFor="pais">Pais:</label>
-          <select name="pais" id="pais">
+          <select name="pais" id="pais" onChange={datosBusqueda} value={pais}>
             <option value="">Seleccionar un pais</option>
             <option value="AR">Argentina</option>
             <option value="CL">Chile</option>
--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -1,27 +1,49 @@
+import axios from 'axios'
 import React, { createContext, useState } from 'react'
 
 const ClimaContext = createContext()
 
 const WeatherProvider = ({ children }) => {
 
+    console.log(import.meta.env.VITE_API_KEY)
+
     const [busqueda, setBusqueda] = useState({
         ciudad: '',
         pais: ''
     })
+    const [resultado, setResultado] = useState({})
 
     const datosBusqueda = (e) => {
-        e.preventDefaul()
         setBusqueda({
             ...busqueda,
             [e.target.name]: e.target.value
         })
     }
 
+    const consultarClima = async (datos) => {
+        try {
+            const {ciudad, pais} = datos
+            const appId = import.meta.env.VITE_API_KEY
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+            // console.log(url)
+            const {data} = await axios(url)
+            const {lat, lon} = data.coord
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const {data: clima} = await axios(urlClima)
+            console.log(clima)
+            setResultado(clima)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <ClimaContext.Provider
             value={{
                 busqueda,
-                datosBusqueda
+                datosBusqueda,
+                consultarClima,
+                resultado
             }}
         >
             {children}
