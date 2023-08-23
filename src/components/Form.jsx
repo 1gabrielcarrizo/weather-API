003a26f67203b597d8a173257e29ab435c0f57e8
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-22 21:06:24 -0300

    editing form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad:</label>
+          <input type="text" id='ciudad' name='ciudad' />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">Pais:</label>
+          <select name="pais" id="pais">
+            <option value="">Seleccionar un pais</option>
+            <option value="AR">Argentina</option>
+            <option value="CL">Chile</option>
+            <option value="BR">Brasil</option>
+            <option value="US">Estados Unidos</option>
+            <option value="ES">España</option>
+            <option value="KR">Corea del Sur</option>
+          </select>
+        </div>
+        <input type="submit" value="Consultar clima" />
+      </form>
+    </div>
   )
 }
 
